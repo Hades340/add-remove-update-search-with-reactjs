1b4dbfc4a185af4a27c009936d41a1f0d0c7a26f
= <=>
2020-07-23 23:33:04 +0700

    add,update,remove,search with reactjs

---
+++ precache-manifest.889c6c84c4eecae7a5e4f6fd40dc079c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee845a7e89047a275e0c819491555ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8f5f23407bbe606ea42",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac11b2c287ac7ac0fabd",
+    "url": "/static/js/2.cf773a6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf773a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f5f23407bbe606ea42",
+    "url": "/static/js/main.2174a694.chunk.js"
+  },
+  {
+    "revision": "cec01d0b9ddad6138e72",
+    "url": "/static/js/runtime-main.5a694172.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
